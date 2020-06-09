d1ca302aa374878527b90d08f05f8933fc51c402
dhintz <dhintz89@gmail.com>
2020-06-09 18:41:36 -0400

    add LikeButton frontent

---
+++ client/src/components/products/LikeButton.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export default class LikeButton extends Component {
+    state = {
+        like: false
+    }
+
+    handleClick = () => {
+        this.setState((prevState) => ({
+            like: !prevState.like  
+        }))
+    }
+
+    render() {
+        const heartStatus = this.state.like ? "filledHeart" : "emptyHeart"
+        return <img class="heartIcon" src={`../../../${heartStatus}.png`} height="20px" onClick={this.handleClick}/>
+    }
+}
--- client/src/components/products/ProductCard.js
+++ client/src/components/products/ProductCard.js
@@ -2,26 +2,18 @@ import React from 'react';
 import AddToCartButton from '../cart/AddToCartButton';
 import Image from 'react-bootstrap/Image';
 import {NavLink} from 'react-router-dom';
+import LikeButton from './LikeButton';
 
-const handleClick = (event, prod) => {
-    // control flow allows the full component to be selected even if a child (i.e. title) is clicked - only use if prod doesn't work
-    // replace the below console logs with React Routing to the productShow component - don't forget about history tracking
-    // if(event.target.className === "product-card") {
-    //     console.log(prod)
-    // } else {
-    //     console.log(event.target.parentElement.id)
-    // }
-    console.log(prod)
-}
 
 const ProductCard = props => {
     return (
-        <div className="product-card" id={props.product.id} onClick={event => handleClick(event, props.product)}>
+        <div className="product-card" id={props.product.id}>
             <NavLink to={`/products/${props.product.id}`} >
                 <Image src={props.product.thumbnail} alt={props.product.title} rounded />
                 <h5>{props.product.title}</h5>
             </NavLink>
             <AddToCartButton class={`add_${props.product.id}_to_cart`} handleCartAdd={props.handleCartAdd} product={props.product} />
+            <LikeButton />
         </div>
     )
 }
