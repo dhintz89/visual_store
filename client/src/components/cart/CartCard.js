8becab6e2d65889dbf6c541890ad73bf8e3ff0a7
dhintz <dhintz89@gmail.com>
2020-07-19 19:44:10 -0400

    add cart visibility and functionality from ProductShow

--- client/src/components/cart/CartCard.js
+++ client/src/components/cart/CartCard.js
@@ -19,7 +19,7 @@ const CartCard = props => {
         <div className="lineItem-card" id={props.lineItem.product_id} onClick={event => handleClick(event, props.lineItem)}>
             <Image src={props.lineItem.thumbnail} alt={props.lineItem.title} thumbnail />
             <span>{props.lineItem.title}</span>
-            <p>Quantity: <br className="breakpoint" />{<button onClick={event => props.handleQtyIncrease(props.lineItem.product_id)} >+</button>} {props.lineItem.quantity} {<button onClick={event => props.handleQtyDecrease(props.lineItem.product_id)}>-</button>}<br/>
+            <p className="cartItemDescription">Quantity: <br className="breakpoint" />{<button onClick={event => props.handleQtyIncrease(props.lineItem.product_id)} >+</button>} {props.lineItem.quantity} {<button onClick={event => props.handleQtyDecrease(props.lineItem.product_id)}>-</button>}<br/>
             Total Price: {props.lineItem.price_total}</p>
             <RemoveFromCartButton handleCartRemove={props.handleCartRemove} lineItem={props.lineItem} />
         </div>
--- client/src/components/products/ProductShow.js
+++ client/src/components/products/ProductShow.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import AddToCartButton from '../cart/AddToCartButton';
+import Cart from '../cart/Cart'
+import {addToCart} from '../../actions/cart';
 
 
 class ProductShow extends Component {
   
+    handleCartAdd = product => {
+        if (this.props.orderLineItems.findIndex(line => line.product_id === product.id) !== -1) {
+            window.alert(`To protect you from making accidental duplicate purchases, you can only add a product to the cart once. \r \r If you would like additional copies, increase Quantity in your Shopping Cart.`)
+        } else {
+            return this.props.addToCart(product)
+        }
+    }
+    
     render() {
         const product = this.props.products.find(pr => pr.id === parseInt(this.props.match.params.prodId))
         return (
@@ -12,13 +22,13 @@ class ProductShow extends Component {
                 <h1>"<u>{product.title}</u>"</h1><br/>
                 <div className="showPageWrapper">
                     <div className="showPageImg"><img src={product.image} /></div><br/>
-                    <h3>${product.price}&nbsp;&nbsp;<AddToCartButton /></h3>
-                    {/* <AddToCartButton class={`add_${props.product.id}_to_cart`} handleCartAdd={props.handleCartAdd} product={props.product} /> */}
+                    <h3 className="price">${product.price}&nbsp;&nbsp;<AddToCartButton class={`add_${product.id}_to_cart`} handleCartAdd={this.handleCartAdd} product={product}/></h3>
                     <p>{product.description}</p>
                     <p className="copyright"><i>Published: {product.publicationDate}<br/>Copyright: {product.copyright}</i></p>
                     <br/>
                     <p>Category: {product.category}</p>
                 </div>
+                <Cart />
             </div>
         )
     }
@@ -26,8 +36,15 @@ class ProductShow extends Component {
 
 const mapStateToProps = state => {
     return {
-        products: state.products.products
+        products: state.products.products,
+        orderLineItems: state.cart
     }
 }
 
-export default connect(mapStateToProps)(ProductShow)
+const mapDispatchToProps = dispatch => {
+    return {
+        addToCart: (product) => {dispatch(addToCart(product))}
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductShow)
