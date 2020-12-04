e0624c535a4e42254b8c5a762e7c4127ea3d3387
dhintz <dhintz89@gmail.com>
2020-12-03 19:52:18 -0500

    update routes and User to remove devise-jwt related content

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users,
   controllers: {
-    registrations: 'registrations',
-    sessions: 'sessions'
+    registrations: :registrations,
+    sessions: :sessions
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
