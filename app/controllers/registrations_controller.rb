295554e9943cbca9f6deffe9bc0f370c73b19da3
dhintz <dhintz89@gmail.com>
2020-12-04 19:54:33 -0500

    remove devise-jwt code and replace with customized devise for Registration Controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,25 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
-    # before_action :configure_permitted_parameters
-    respond_to :json
+
+  def create
+    user = User.new(sign_up_params)
+
+    if user.save
+      @current_user = user
+      render json: @current_user, serializer: SessionSerializer
+    else
+      render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
+    end
+  end
   
-    
-    # def create
-    #   build_resource(sign_up_params)
-    #   binding.pry
-    #   sign_in
-    #   resource.save
-    #   binding.pry
-    #   render_resource(resource)
-    # end
-
-    protected
-
-    # def configure_permitted_parameters
-    #     binding.pry
-    #     devise_parameter_sanitizer.permit(:sign_up, keys: [:admin])
-    # end
-
-    # def sign_up_params
-    #     params.require(:registration).permit(:email, :password, :password_confirmation)
-    # end
   end
